7c57f7b066f4313a96a3b9f97c6dc61121ddc3d1
seyyed hassan moosavi nazar <73283752+moosavinazar@users.noreply.github.com>
2021-12-05 22:44:01 +0330

    use attribute binding to set style with object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   isCorrect: boolean = false;
+
+  styles = {};
+
+  ngOnInit(): void {
+    this.styles = {
+      'font-size': '2rem',
+      'color': 'red'
+    }
+  }
 }
